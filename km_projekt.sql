2d6abb112da490ebc8f9dd0f1bc5c584a335af09
Krzysztof Tyszkiewicz <rysiu352@gmail.com>
2023-11-24 20:51:04 +0100

    new database file

--- km_projekt.sql
+++ km_projekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 23 Lis 2023, 16:13
+-- Czas generowania: 24 Lis 2023, 20:48
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.2.0
 
@@ -20,24 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `km_projekt`
 --
-
-DELIMITER $$
---
--- Funkcje
---
-CREATE DEFINER=`root`@`localhost` FUNCTION `try_log_in` (`email` VARCHAR(40), `password` VARCHAR(256)) RETURNS INT(11)  BEGIN
-DECLARE user_id INT;
-
-SELECT user.id INTO user_id from user WHERE user.email=email AND user.password=password;
-
-if user_id is null then
-RETURN -1;
-ELSE
-RETURN user_id;
-END IF;
-END$$
-
-DELIMITER ;
+CREATE DATABASE IF NOT EXISTS `km_projekt` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `km_projekt`;
 
 -- --------------------------------------------------------
 
